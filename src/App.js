b615ea69d0ff0e163ba6d9923692ea9828127e87
Your Name <bt.asbai@gmail.com>
2020-06-29 21:11:13 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import { Route, Switch, useHistory,
-  useLocation } from "react-router-dom";
+import { Route, Switch, useHistory } from "react-router-dom";
 import MyCardWrapper from "./MyCardWrapper";
 import CreateCard from "./CreateCard";
 import BottomNav from "./BottomNav";
-export default App
+
 
 const App = () => {
   const [cardContent, setCardContent] = useState([
@@ -38,16 +37,15 @@ const App = () => {
           <CreateCard setCardContent={setCardContent} />
         </Route>
         <Route path="/login">
-          <LoginPage/>
+         
         </Route>
-        <PrivateRoute path="/home">
-          <HomePage/>
-        </PrivateRoute>
+        <Route path="/home">
+        </Route>
       </Switch>
       <BottomNav />
     </>
-  );
-};
+  )
+}
 
 const fakeAuth = {
   isAuthenticated: false,
@@ -67,58 +65,16 @@ function AuthButton() {
   return fakeAuth.isAuthenticated ? (
     <p>
       Welcome!{" "}
-      <button
-        onClick={() => {
+      <button onClick={() => {
           fakeAuth.signout(() => history.push("/"));
-        }}
-      >
-        Sign out
-      </button>
+          }}>Sign out</button>
     </p>
   ) : (
     <p>You are not logged in.</p>
   );
 
-  function PrivateRoute({ children, ...rest }) {
-    return (
-      <Route
-        {...rest}
-        render={({ location }) =>
-          fakeAuth.isAuthenticated ? (
-            children
-          ) : (
-            <Redirect
-              to={{
-                pathname: "/login",
-                state: { from: location }
-              }}
-            />
-          )
-        }
-      />
-    );
-  }
-function HomePage() {
-  return <h3>Public</h3>;
-}
-
-function LoginPage() {
-  let history = useHistory();
-  let location = useLocation();
-
-  let { from } = location.state || { from: { pathname: "/" } };
-  let login = () => {
-    fakeAuth.authenticate(() => {
-      history.replace(from);
-    });
-  };
-
-  return (
-    <div>
-      <p>You must log in to view the page at {from.pathname}</p>
-      <button onClick={login}>Log in</button>
-    </div>
-  );
-}};
 
 
+};
+
+export default App
